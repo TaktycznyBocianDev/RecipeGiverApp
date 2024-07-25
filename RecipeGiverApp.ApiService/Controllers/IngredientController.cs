a4dec22f20a40813ccd2e8fafd70ae7e73939239
Stanisław Franczak <112652279+TaktycznyBocianDev@users.noreply.github.com>
2024-07-25 23:02:40 +0200

    CRUD for Ingredients

--- RecipeGiverApp.ApiService/Controllers/IngredientController.cs
+++ RecipeGiverApp.ApiService/Controllers/IngredientController.cs
@@ -53,5 +53,38 @@ namespace RecipeGiverApp.ApiService.Controllers
                 return StatusCode(500, "Internal server error");
             }
         }
+
+        [HttpPut("UpdateIngredient")]
+        public async Task<ActionResult> UpdateIngredientNameAsync(int ingredientId, string newName)
+        {
+            try
+            {
+                var result = await _ingredientService.UpdateIngredientNameAsync(ingredientId, newName);
+                if (result == 0) return NotFound("No ingredient updated. Are Id and New Name valid?");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while updating the ingredient");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpDelete("DeleteIngredient")]
+        public async Task<ActionResult> DeleteIngredientAsync(string ingredientName)
+        {
+            try
+            {
+                var result = await _ingredientService.DeleteIngredientAsync(ingredientName);
+                if (result == 0) return NotFound("No ingredient deleted. Is name valid?");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while updating the ingredient");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
     }
 }
--- ReciveGiverApp.BL/Services/IngredientService.cs
+++ ReciveGiverApp.BL/Services/IngredientService.cs
@@ -4,6 +4,7 @@ using ReciveGiverApp.Database.Data;
 using System.Data;
 using Dapper;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace ReciveGiverApp.BL.Services
 {
@@ -12,6 +13,8 @@ namespace ReciveGiverApp.BL.Services
     {
         public Task<List<Ingredient>> GetIngredientsAsync(int? Id = null, string? Name = null);
         public Task<int> CreateIngredientAsync(Ingredient ingredient);
+        public Task<int> UpdateIngredientNameAsync(int ingredientId, string newName);
+        public Task<int> DeleteIngredientAsync(string newName);
 
     }
 
@@ -54,7 +57,7 @@ namespace ReciveGiverApp.BL.Services
                     var categories = result.ToList();
                     connection.Close();
                     return categories;
-                    
+
                 }
             }
             catch (Exception ex)
@@ -79,6 +82,50 @@ namespace ReciveGiverApp.BL.Services
                     return result;
                 }
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return 0;
+            }
+        }
+
+        public async Task<int> UpdateIngredientNameAsync(int ingredientId, string newName)
+        {
+            try
+            {
+                using (IDbConnection connection = _connectionManager.CreateConnection())
+                {
+                    connection.Open();
+
+                    string sql = "UPDATE Ingredients SET IngredientName = @NewName WHERE IngredientID = @IngredientId;";
+
+                    var result = await connection.ExecuteAsync(sql, new { NewName = newName, IngredientId = ingredientId });
+                    connection.Close();
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return 0;
+            }
+        }
+
+        public async Task<int> DeleteIngredientAsync(string ingredientName)
+        {
+            try
+            {
+                using (IDbConnection connection = _connectionManager.CreateConnection())
+                {
+                    connection.Open();
+
+                    string sql = "DELETE FROM Ingredients WHERE IngredientName = @ingredientName;";
+
+                    var result = await connection.ExecuteAsync(sql, new { ingredientName });
+                    connection.Close();
+                    return result;
+                }
             }
             catch (Exception ex)
             {
