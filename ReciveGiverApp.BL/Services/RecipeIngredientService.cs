37c3d4a1013d1ad269e6e5565c382b74158d85c5
Stanisław Franczak <112652279+TaktycznyBocianDev@users.noreply.github.com>
2024-07-30 17:37:07 +0200

    R from Crud for RecipeIngredients

--- ReciveGiverApp.BL/Services/RecipeIngredientService.cs
+++ ReciveGiverApp.BL/Services/RecipeIngredientService.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReciveGiverApp.Database.Data;
 using ReciveGiverApp.Models.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +15,89 @@ namespace ReciveGiverApp.BL.Services
 
     public interface IRecipeIngredientService
     {
-        public Task<List<Recipe>> GetRecipesWithIngredientsAsync(string? Name = null, Recipe? recipe = null);
+        public Task<Recipe> GetRecipesWithIngredientsAsync(string Name);
+        public Task<List<Recipe>> GetRecipesWithIngredientsAsync();
         public Task<int> CreateRecipesWithCorespondingIngredientsAsync(Recipe recipe, Ingredient[] ingredients);
         public Task<int> UpdateIngredientNameAsync(int ingredientId, string newName);
         public Task<int> DeleteIngredientAsync(string newName);
     }
     public class RecipeIngredientService : IRecipeIngredientService
     {
+        private readonly ConnectionManager _connectionManager;
         private readonly IIngredientService _ingredientService;
         private readonly IRecipeService _recipeService;
+        private readonly ILogger _logger;
 
-        public RecipeIngredientService(IIngredientService ingredientService, IRecipeService recipeService)
+        public RecipeIngredientService(ConnectionManager connectionManager, IIngredientService ingredientService, IRecipeService recipeService, ILogger logger)
         {
+            _connectionManager = connectionManager;
             _ingredientService = ingredientService;
             _recipeService = recipeService;
+            _logger = logger;
         }
+
+        public async Task<List<Recipe>> GetRecipesWithIngredientsAsync(string Name = "")
+        {
+
+            try
+            {
+                var recipes = _recipeService.GetRecipesAsync(Name).GetAwaiter().GetResult();
+                if (recipes == null) throw new Exception($"No recipes were found.");
+
+                using (IDbConnection connection = _connectionManager.CreateConnection())
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM RecipeIngredients WHERE RecipeID = @RecipeID";
+
+                    foreach (var recipe in recipes)
+                    {
+                        var result = await connection.QueryAsync<RecipeIngredientDTO>(sql, new { RecipeID = recipe.RecipeID});
+                        
+                        List<RecipeIngredientDTO> recipeIngredients = result.ToList();
+                        List<Ingredient> ingredients = new List<Ingredient>();
+
+                        foreach (var item in recipeIngredients)
+                        {
+                            var nextIgredient = _ingredientService.GetIngredientsAsync(item.IngredientID).GetAwaiter().GetResult();
+                            if (nextIgredient == null) break;
+
+                            Ingredient ing = nextIgredient[0];
+                            ingredients.Add(ing);
+                        }
+                        _recipeService.GetRecipesAsync();
+                    }
+
+                    connection.Close();
+                   
+                }
+                return recipes;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return 0;
+            }
+
+           
+
+            
+
+            
+
+
+            Recipe requestedRecipe = _recipeService.GetRecipesAsync(Name).GetAwaiter().GetResult()[0];
+            if (requestedRecipe != null) throw new Exception($"There is no Recipe with name {Name}");
+
+
+        }
+
+        public Task<List<Recipe>> GetRecipesWithIngredientsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
 
 
         public Task<int> CreateRecipesWithCorespondingIngredientsAsync(Recipe recipe, Ingredient[] ingredients)
@@ -38,14 +110,13 @@ namespace ReciveGiverApp.BL.Services
             throw new NotImplementedException();
         }
 
-        public Task<List<Recipe>> GetRecipesWithIngredientsAsync(string? Name = null, Recipe? recipe = null)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public Task<int> UpdateIngredientNameAsync(int ingredientId, string newName)
         {
             throw new NotImplementedException();
         }
+
+        
     }
 }
---
+++ ReciveGiverApp.Models/Models/RecipeIngredientDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReciveGiverApp.Models.Models
+{
+    public class RecipeIngredientDTO
+    {
+        public int RecipeID { get; set; }
+        public int IngredientID { get; set; }
+        public string Quantity { get; set; } = string.Empty;
+    }
+}
